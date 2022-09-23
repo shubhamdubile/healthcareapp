a8fcdf191330fa61f1aee9d9001b4bdbd4b79cf0
shubhamdubile <66626803+shubhamdubile@users.noreply.github.com>
2022-09-24 01:27:33 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -13,6 +13,40 @@ import tensorflow as tf
 #from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 # from statistics import mode
 
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+st.markdown("""
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <div class="container-fluid">
+            <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+            <a class="navbar-brand" href="#">
+                <img src="https://www.dailyrounds.org/blog/wp-content/uploads/2015/05/caduceus.jpg" width="30" height="30" alt="">
+              </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page" href="https://healthcareapp0.herokuapp.com/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" href="https://healthcareapp0.herokuapp.com/about">Analysis</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page" href="https://healthcareapp0.herokuapp.com/heart">Heart</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page" href="#">Brain</a>
+                    </li>
+                </ul>   
+            </div>
+        </div>
+    </nav>
+""", unsafe_allow_html=True)
+
+
 def predict(image):
     classifier_model = "vgg19_23Sep.h5"
     IMAGE_SHAPE = (224, 224,3)
@@ -25,16 +59,16 @@ def predict(image):
     # test_image = np.expand_dims(test_image, axis=0)
     test_image = test_image.reshape(1,224,224,3)
     class_names = [
-          'glioma',
-          'no',
-          'Menginioun', 
-          'Pituratry'
+          'You have Glioma Tumor',
+          'Do not worry you do not have Tumor',
+          'You have Meningioma Tumor', 
+          'You have Pituitary Tumor'
           ]
     predictions = model.predict(test_image)
     #scores = tf.nn.softmax(predictions[0])
     #scores = scores.numpy()
     scores=np.argmax(predictions, axis=1)
-    st.text(scores[0])
+    #st.text(scores[0])
     results = class_names[scores[0]]
 
     
@@ -42,7 +76,7 @@ def predict(image):
     return results
 
 uploaded_file = st.file_uploader("Choose a image file")
-st.text(uploaded_file)
+
 
 if uploaded_file  is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
